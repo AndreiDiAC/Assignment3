3719b0e603f6a3f4b9ee73b01e3ef881d2738cf0
AndreiDiaconu <diaconuandreicristian@gmail.com>
2021-06-07 12:41:12 -0500

    Finish

--- src/test/java/Assignment3.java
+++ src/test/java/Assignment3.java
@@ -2,18 +2,17 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Alert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+
 public class Assignment3 {
 
 
@@ -69,11 +68,11 @@ public class Assignment3 {
         @Test
         public void Test2_General_Information() throws InterruptedException {
             Thread.sleep(2000);
-            for (int f = 1; f <= 2; f++) {
+            for (int f = 1; f <= 2; f++) {           //for loop for pages
 
 
                 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-                for (int i = 1; i <= 25; i++) {
+                for (int i = 1; i <= 25; i+=5) {      // for loop for each car
                     WebElement car = driver.findElement(By.xpath("//tbody[@class='grid-body']/tr[" + i + "]"));
                     car.click();
 
@@ -115,84 +114,120 @@ public class Assignment3 {
             driver.findElement(By.xpath("//button[@type='submit']")).click();
             Thread.sleep(3000);
 
-            String findEvent = driver.findElement(By.xpath("//div[@class='items list-box list-shaped']/div/div/div/div[4]/div")).getText();
-            System.out.println(findEvent);
+//            String findEvent = driver.findElement(By.xpath("//div[@class='items list-box list-shaped']/div/div/div/div[4]/div[1]")).getText();
+            String findEvent = driver.findElement(By.xpath("//div[@class='items list-box list-shaped']")).getText();
 
             Thread.sleep(3000);
 
-            Assert.assertEquals(findEvent,sendKey);
+            Assert.assertTrue(findEvent.contains(sendKey));
 
         }
 
-    @Test
-    public void Test4_Reset_Settings() throws InterruptedException {
-        Thread.sleep(2000);
-        WebElement userName = driver.findElement(By.xpath("//i[@class='fa-caret-down']"));
-        userName.click();
 
-        Thread.sleep(2000);
-        WebElement myConfiguration = driver.findElement(By.xpath("//ul[@role='menu']/li[2]"));
-        myConfiguration.click();
+        @Test
+        public void Test4_Vehicle_Page() throws InterruptedException {
 
-        Thread.sleep(2000);
-        WebElement firstCheckBox = driver.findElement(By.xpath("//input[@data-ftid='localization_oro_locale___locale_use_parent_scope_value']"));
-        firstCheckBox.click();
+        Thread.sleep(1000);
+        driver.navigate().back();
 
-        Thread.sleep(2000);
-        WebElement locateElem = driver.findElement(By.xpath("//select[@data-ftid='localization_oro_locale___locale_value']"));
-        locateElem.click();
-        Select select = new Select(locateElem);
-        select.selectByVisibleText("Croatian");
+        Thread.sleep(1000);
+        WebElement gridSettingButton = driver.findElement(By.xpath("//i[@class='fa-cog hide-text']"));
+        gridSettingButton.click();
 
-        String actualCountry = select.getFirstSelectedOption().getText();
-        String expectedCountry = "Croatian";
+        Thread.sleep(1000);
+        WebElement idCheckbox = driver.findElement(By.xpath("//input[starts-with(@id,'column')][1]"));
+        idCheckbox.click();
+        Thread.sleep(1000);
+        Assert.assertTrue(idCheckbox.isSelected());
 
-        Assert.assertEquals(actualCountry,expectedCountry);
+        List<WebElement> listOfCheckbox = driver.findElements(By.xpath("//tbody//td/input"));
+        for( int i = 1; i < listOfCheckbox.size(); i++){
+            Assert.assertTrue(listOfCheckbox.get(i).isSelected());
+        }
+
+        Thread.sleep(1000);
+        WebElement closeDropdown = driver.findElement(By.xpath("//div[@class='dropdown-menu']//span[@class='close']"));
+        closeDropdown.click();
+
+        Thread.sleep(1000);
+        WebElement refreshButton = driver.findElement(By.cssSelector("[title*='Reset']>i"));
+        refreshButton.click();
+
+        Thread.sleep(1000);
+        gridSettingButton.click();
+
+        Thread.sleep(1000);
+        Assert.assertTrue(!idCheckbox.isSelected());
 
 
-
-
-        Thread.sleep(2000);
-        WebElement saveButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        saveButton.click();
-
-
-        Thread.sleep(4000);
-
-        WebElement resetButton = driver.findElement(By.xpath("//button[@type='reset']"));
-        resetButton.click();
-
-        Thread.sleep(2000);
-        WebElement acceptAlert = driver.findElement(By.xpath("//a[@class='btn ok btn-primary']"));
-        acceptAlert.click();
-
-        Thread.sleep(2000);
-
-        WebElement save = driver.findElement(By.xpath("//button[@type='submit']"));
-        save.click();
-
-        Thread.sleep(2000);
-        WebElement firstBox = driver.findElement(By.xpath("//input[@data-ftid='localization_oro_locale___locale_use_parent_scope_value']"));
-        firstBox.click();
-
-        Thread.sleep(2000);
-        WebElement locateElement = driver.findElement(By.xpath("//select[@data-ftid='localization_oro_locale___locale_value']"));
-        Select select1 = new Select(locateElement);
-        String actualResult = select1.getFirstSelectedOption().getText();
-        String expectedResult = "English";
-
-        Assert.assertEquals(actualResult,expectedResult);
+        for( int i = 1; i < listOfCheckbox.size(); i++){
+            Assert.assertTrue(listOfCheckbox.get(i).isSelected());
+        }
 
 
 
 
+        }
 
-
-
-//        List<WebElement> checkBoxList = driver.findElements(By.xpath("//input[@value='1']"));
-//        Thread.sleep(3000);
-//        System.out.println(checkBoxList.size());
-    }
+//    @Test
+//    public void Test4_Reset_Settings() throws InterruptedException {
+//        Thread.sleep(2000);
+//        WebElement userName = driver.findElement(By.xpath("//i[@class='fa-caret-down']"));
+//        userName.click();
+//
+//        Thread.sleep(2000);
+//        WebElement myConfiguration = driver.findElement(By.xpath("//ul[@role='menu']/li[2]"));
+//        myConfiguration.click();
+//
+//        Thread.sleep(2000);
+//        WebElement firstCheckBox = driver.findElement(By.xpath("//input[@data-ftid='localization_oro_locale___locale_use_parent_scope_value']"));
+//        firstCheckBox.click();
+//
+//        Thread.sleep(2000);
+//        WebElement locateElem = driver.findElement(By.xpath("//select[@data-ftid='localization_oro_locale___locale_value']"));
+//        locateElem.click();
+//        Select select = new Select(locateElem);
+//        select.selectByVisibleText("Croatian");
+//
+//        String actualCountry = select.getFirstSelectedOption().getText();
+//        String expectedCountry = "Croatian";
+//
+//        Assert.assertEquals(actualCountry,expectedCountry);
+//
+//
+//        Thread.sleep(2000);
+//        WebElement saveButton = driver.findElement(By.xpath("//button[@type='submit']"));
+//        saveButton.click();
+//
+//
+//        Thread.sleep(4000);
+//
+//        WebElement resetButton = driver.findElement(By.xpath("//button[@type='reset']"));
+//        resetButton.click();
+//
+//        Thread.sleep(2000);
+//        WebElement acceptAlert = driver.findElement(By.xpath("//a[@class='btn ok btn-primary']"));
+//        acceptAlert.click();
+//
+//        Thread.sleep(2000);
+//
+//        WebElement save = driver.findElement(By.xpath("//button[@type='submit']"));
+//        save.click();
+//
+//        Thread.sleep(2000);
+//        WebElement firstBox = driver.findElement(By.xpath("//input[@data-ftid='localization_oro_locale___locale_use_parent_scope_value']"));
+//        firstBox.click();
+//
+//        Thread.sleep(2000);
+//        WebElement locateElement = driver.findElement(By.xpath("//select[@data-ftid='localization_oro_locale___locale_value']"));
+//        Select select1 = new Select(locateElement);
+//        String actualResult = select1.getFirstSelectedOption().getText();
+//        String expectedResult = "English";
+//
+//        Assert.assertEquals(actualResult,expectedResult);
+//
+//
+//    }
 
     }
 
